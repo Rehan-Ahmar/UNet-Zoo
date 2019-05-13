b0d9c916ef8f039cbd1c22968b777898d80f5450
Rehan Ahmar Sk <Rehan.Ahmar@ad.infosys.com>
2019-05-13 16:12:45 +0530

    Updating filed

--- binary-implementation/dilate_text.py
+++ binary-implementation/dilate_text.py
@@ -3,32 +3,23 @@ import os
 import cv2
 import numpy as np
 
-
 def dilate_text(args):
     for file_name in os.listdir(args.input_dir):
         input_file_path = os.path.join(args.input_dir, file_name)
         output_file_path = os.path.join(args.output_dir, file_name)
         dilate_text_in_image(input_file_path, output_file_path)
 
-
 def dilate_text_in_image(input_file_path, output_file_path):
     image = cv2.imread(input_file_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # grayscale
-    _, thresh = cv2.threshold(
-        gray, 150, 255, cv2.THRESH_BINARY_INV)  # threshold
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY_INV)
     kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-    dilated = cv2.dilate(thresh, kernel, iterations=5)  # dilate
-
+    dilated = cv2.dilate(thresh, kernel, iterations=5)
     cv2.imwrite(output_file_path, dilated)
 
-
 if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser(
-        description='Detects text in images')
+    parser = argparse.ArgumentParser(description='Detects text in images')
     parser.add_argument('--input_dir', help='Input directory', required=True)
-    parser.add_argument(
-        '--output_dir', help='Output data directory', required=False)
+    parser.add_argument('--output_dir', help='Output data directory', required=False)
     FLAGS, unparsed = parser.parse_known_args()
-
     dilate_text(FLAGS)
